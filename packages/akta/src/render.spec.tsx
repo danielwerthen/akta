9e6a842c49e34dc94266371a46c125690b0f21bd
Daniel Lichtenstein <danielwerthen@gmail.com>
2021-12-05 16:22:00 +0100

    Prepared DOM nodes

--- packages/akta/src/render.spec.tsx
+++ packages/akta/src/render.spec.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource . */
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { AktaNode } from '../dist';
 import { render } from './render';
+import { useRender } from './use-render';
 
 describe('render', () => {
   let parent: HTMLElement;
@@ -19,6 +21,30 @@ describe('render', () => {
   });
   it('should rendering akta blueprint', () => {
     doRender(<p>Blueprint</p>);
+    expect(parent).toMatchSnapshot();
+  });
+  it('should use prepared rendering akta blueprint', () => {
+    function Component({
+      children,
+      order,
+    }: {
+      order: Observable<number[]>;
+      children: AktaNode[];
+    }) {
+      const doRender = useRender();
+      const arr = children.map(doRender);
+      return <div>{order.pipe(map(idx => idx.map(i => arr[i])))}</div>;
+    }
+    const subj = new BehaviorSubject([0, 1, 2]);
+    doRender(
+      <Component order={subj}>
+        <div>Alpha</div>
+        <div>Beta</div>
+        <div>Theta</div>
+      </Component>
+    );
+    expect(parent).toMatchSnapshot();
+    subj.next([2, 1, 0]);
     expect(parent).toMatchSnapshot();
   });
 });
