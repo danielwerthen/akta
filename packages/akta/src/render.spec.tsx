c0576b0119d8e3e60dc331618e0e04cf5b8cd08b
Daniel Lichtenstein <danielwerthen@gmail.com>
2021-12-05 20:56:48 +0100

    Add more tests

--- packages/akta/src/render.spec.tsx
+++ packages/akta/src/render.spec.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource . */
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
 import { AktaNode } from '../dist';
 import { render } from './render';
 import { useRender } from './use-render';
@@ -35,16 +35,70 @@ describe('render', () => {
       const arr = children.map(doRender);
       return <div>{order.pipe(map(idx => idx.map(i => arr[i])))}</div>;
     }
+    const subfn = jest.fn();
+    subfn.mockReturnValue('sub-fn');
+    function SubComponent() {
+      return <p>{subfn()}</p>;
+    }
     const subj = new BehaviorSubject([0, 1, 2]);
     doRender(
       <Component order={subj}>
-        <div>Alpha</div>
-        <div>Beta</div>
-        <div>Theta</div>
+        <div>
+          Alpha
+          <SubComponent />
+        </div>
+        <div>
+          Beta
+          <SubComponent />
+        </div>
+        <div>
+          Theta
+          <SubComponent />
+        </div>
       </Component>
     );
     expect(parent).toMatchSnapshot();
     subj.next([2, 1, 0]);
+    expect(parent).toMatchSnapshot();
+    subj.next([0, 1, 0, 2, 0]);
+    expect(parent).toMatchSnapshot();
+    expect(subfn.mock.calls.length).toEqual(3);
+  });
+  it('should allow async elements in prepared nodes', () => {
+    function Component({
+      children,
+      order,
+    }: {
+      order: Observable<number[]>;
+      children: AktaNode[];
+    }) {
+      const doRender = useRender();
+      const arr = children.map(doRender);
+      return <div>{order.pipe(map(idx => idx.map(i => arr[i])))}</div>;
+    }
+    const subject = new Subject<number>();
+    function SubComponent() {
+      return <p>{subject}</p>;
+    }
+    const subj = new BehaviorSubject([1, 1, 0]);
+    doRender(
+      <Component order={subj}>
+        <div>
+          Alpha
+          <SubComponent />
+        </div>
+        <div>
+          Beta
+          <SubComponent />
+        </div>
+        <div>
+          Theta
+          <SubComponent />
+        </div>
+      </Component>
+    );
+    expect(parent).toMatchSnapshot();
+    subject.next(44);
     expect(parent).toMatchSnapshot();
   });
 });
